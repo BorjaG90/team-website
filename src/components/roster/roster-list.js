9f52414ec3e94e55ffac7cba5aecba452fcf2aa8
BorjaG90 <borjag90dev@gmail.com>
2021-09-28 13:41:39 +0200

    feat: add modal player info #9

--- src/components/roster/roster-list.js
+++ src/components/roster/roster-list.js
@@ -1,12 +1,37 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 import { LitElement, html, css, nothing } from 'lit';
 
 import './player-line.js';
+import '@borjag90dev/lit-modal';
 
 const errorHandler = response => {
   if (!response.ok) {
     return { error: response.statusText, errorCode: response.status };
   }
   return response.json();
+};
+
+const drawModalContent = player => {
+  let content = '';
+  for (const property in player) {
+    content += `<span>
+      <b class="first-lvl"> ${property}: </b>`;
+    if (property === '_highlightResult') {
+      const highlight = player[property];
+      // content += '<br />';
+      for (const attribute in highlight) {
+        content += `<br /><b class="second-lvl"><i>${attribute}: </i></b>`;
+        for (const attrProp in highlight[attribute]) {
+          content += `<br /><b class="third-lvl">${attrProp}: </b>${highlight[attribute][attrProp]}`;
+        }
+      }
+    } else {
+      content += `${player[property]}`;
+    }
+    content += `</span><br/>`;
+  }
+  return content;
 };
 
 class RosterList extends LitElement {
@@ -67,15 +92,41 @@ class RosterList extends LitElement {
     }
   }
 
+  /** Muestra la ventana modal con los datos completos de la fila */
+  openModal(playerInfo) {
+    const modal = this.shadowRoot.querySelector('lit-modal');
+    modal.innerHTML = `<style>
+    .btn-close {
+      color: var(--lit-modal-btn-color, #999);
+      padding: 1rem;
+      float: right;
+      font-size: 28px;
+      font-weight: bold;
+    }
+
+    .btn-close:hover,
+    .btn-close:focus {
+      color: var(--lit-modal-text-color, #000);
+      text-decoration: none;
+      cursor: pointer;
+    }
+  </style>
+  <span slot="modal-close" class="btn-close">&times;</span>`;
+    modal.innerHTML += drawModalContent(playerInfo);
+    modal.setModalShow(true);
+  }
+
   render() {
     return this.list.length > 0
       ? html`
+          <lit-modal close-backdrop id="player-modal"></lit-modal>
           <player-line class="header" .player=${this.header}></player-line>
           ${this.list.map(
             player =>
               html`<player-line
                 class="player"
                 .player="${player}"
+                @click="${() => this.openModal(player)}"
               ></player-line>`
           )}
         `
